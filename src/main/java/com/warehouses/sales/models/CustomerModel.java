0ab7511a4ae29657ebc53e5b3f6a52ca08f6355a
tamer <tamer.dev.alex@gmail.com>
2023-04-02 22:51:22 +0300

    add new model for customer

---
+++ src/main/java/com/warehouses/sales/models/CustomerModel.java
@@ -0,0 +1,77 @@
+package com.warehouses.sales.models;
+
+import java.util.Objects;
+
+public class CustomerModel {
+    public CustomerModel(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public CustomerModel() {
+
+    }
+
+    private Integer id;
+    private String name;
+    private String email ;
+    private Integer age;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomerModel that = (CustomerModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(email, that.email) && Objects.equals(age, that.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
